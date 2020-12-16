efc0388aa0434ce68e1da83a31ffda6a695ff8e4
Matilde Moreira <33351829+MatildeMoreira@users.noreply.github.com>
2020-12-16 01:54:10 +0000

    Update RI.sql

--- Parte 3/delivery-03-30/RI.sql
+++ Parte 3/delivery-03-30/RI.sql
@@ -10,17 +10,33 @@
  * Matilde, 84137
  * Duarte, 94192
  ***********************************************************************/
+
 -- DROP TRIGGER removes an existing trigger definition.
-DROP TRIGGER IF EXISTS check_pv_busbar_pbbid ON transformer CASCADE;
-DROP TRIGGER IF EXISTS check_sv_busbar_sbbid ON transformer CASCADE;
-DROP TRIGGER IF EXISTS check_analystSubstation ON analyses CASCADE;
+
+ -- We also implemented the IC-1 and IC-2 with triggers but its in comments, since we found an easiest way without triggers
+ -- DROP TRIGGER IF EXISTS check_pv_busbar_pbbid ON transformer CASCADE;
+ -- DROP TRIGGER IF EXISTS check_sv_busbar_sbbid ON transformer CASCADE;
+
+DROP TRIGGER IF EXISTS check_analystSubstation ON analyses CASCADE; -- (IC-5)
 
 -- DROP FUNCTION removes the definition of an existing function.
-DROP FUNCTION IF EXISTS check_pv_busbar_pbbid_proc() CASCADE;
-DROP FUNCTION IF EXISTS check_sv_busbar_sbbid_proc() CASCADE;
-DROP FUNCTION IF EXISTS check_analystSubstation_proc() CASCADE;
+
+ -- We also implemented the IC-1 and IC-2 with triggers but its in comments, since we found an easiest way without triggers
+-- DROP FUNCTION IF EXISTS check_pv_busbar_pbbid_proc() CASCADE;
+-- DROP FUNCTION IF EXISTS check_sv_busbar_sbbid_proc() CASCADE;
+DROP FUNCTION IF EXISTS check_analystSubstation_proc() CASCADE; -- (IC-5)
+
+
+-- (IC-1) AND (IC-2)
+ALTER TABLE busbar ADD CONSTRAINT Unique_PairVoltageID UNIQUE (id,voltage);
 
  -- (IC1) For every transformer, pv must correspond to the voltage of the busbar identified by pbbid.
+ALTER TABLE transformer DROP CONSTRAINT transformer_pbbid_fkey;
+ALTER TABLE transformer ADD CONSTRAINT transformer_pbbid_fkey FOREIGN KEY (pbbid,pv) REFERENCES busbar (id,voltage);
+
+
+-- Another Solution (IC-1) using Trigger
+/*
 CREATE OR REPLACE FUNCTION check_pv_busbar_pbbid_proc()
 RETURNS TRIGGER AS
 $BODY$
@@ -34,13 +50,21 @@ BEGIN
 END;
 $BODY$ LANGUAGE plpgsql;
 
+
 CREATE TRIGGER check_pv_busbar_pbbid
     BEFORE INSERT ON transformer
     FOR EACH ROW EXECUTE procedure check_pv_busbar_pbbid_proc();
 
+*/
+
 
 
 -- (IC2) For every transformer, sv must correspond to the voltage of the busbar identified by sbbid.
+ALTER TABLE transformer DROP CONSTRAINT transformer_sbbid_fkey;
+ALTER TABLE transformer ADD CONSTRAINT transformer_sbbid_fkey FOREIGN KEY (sbbid,sv) REFERENCES busbar (id,voltage);
+
+-- Another Solution (IC-2) Using Trigger
+/*
 CREATE OR REPLACE FUNCTION check_sv_busbar_sbbid_proc()
 RETURNS TRIGGER AS
 $BODY$
@@ -58,11 +82,12 @@ $BODY$ LANGUAGE plpgsql;
 CREATE TRIGGER check_sv_busbar_sbbid
     BEFORE UPDATE OR INSERT ON transformer
     FOR EACH ROW EXECUTE procedure check_sv_busbar_sbbid_proc();
-
+*/
 
 -- (IC5) For every analysis concerning a transformer, the name, address values cannot coincide with sname, saddress
 -- values of the substation where the transformer is located (i.e., gpslat and gpslong have the same values in
 -- transformer and substation).
+
 CREATE OR REPLACE FUNCTION check_analystSubstation_proc()
 RETURNS TRIGGER AS
 $BODY$
